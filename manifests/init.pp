441ff8e943a93c812be4ac403c114808da08976d
Junaid Syed <junaid@kandiaro.net>
2015-07-14 06:43:50 -0400

    Style guide updates for puppet code

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,17 +5,17 @@
 # === 
 
 class hosts(
-	$file = $hosts::params::file,
-	$hosts = {entry => {ip => '127.0.0.0', host => 'localhost localhost.localdomain', alias => 'localdomain'}}
+ $file = $hosts::params::file,
+ $hosts = {entry => {ip => '127.0.0.0',host => 'localhost localhost.localdomain',alias => 'localdomain'}}
 		
 ) inherits hosts::params {
-	validate_hash($hosts)
-	file{$file:
-		ensure => present,
-		owner  => 'root',
-		group  => 'root',
-		mode   => '0644',
-		content => template('hosts/hosts.erb'),
-	}
+ validate_hash($hosts)
+  file{$file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('hosts/hosts.erb'),
+  }
 }
 
